73e8c8cb71bce8301d741395fac42446e756ef52
zyx <1376672363@qq.com>
2021-05-02 19:37:07 +0800

    搭建home页面,修复从home跳转到其他页面菜单不打开的bug

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import { Menu } from 'antd'
 import { WechatOutlined } from '@ant-design/icons'
@@ -7,49 +8,58 @@ import routeConfig from '../../config/routeConfig'
 import './index.less'
 const { SubMenu } = Menu
 
-function LeftNav (_window) {
+const generateMenuItem = (menuConfig, setOpenKeys) => {
+    return menuConfig.reduce((preValue, menuItem) => {
+        // 不存在子菜单
+        if(!menuItem.children){
+            preValue.push((
+                <Menu.Item key={menuItem.path} icon={menuItem.icon}>
+                    <Link to={menuItem.path}>{menuItem.title}</Link>
+                </Menu.Item>
+            ))
+        }else{
+            preValue.push((
+                <SubMenu 
+                    key={menuItem.path} 
+                    icon={menuItem.icon} 
+                    title={menuItem.title}
+                    onTitleClick={e => setOpenKeys(e.key)}
+                >
+                    {generateMenuItem(menuItem.children)}
+                </SubMenu>
+            ))
+        }
+        // 存在子菜单，先创建子菜单，再创建菜单项
+        return preValue
+    }, [])
+}
 
-    const generateMenuItem = menuConfig => {
-        return menuConfig.reduce((initValue, menuItem) => {
-            // 不存在子菜单
-            if(!menuItem.children){
-                initValue.push((
-                    <Menu.Item key={menuItem.path} icon={menuItem.icon}>
-                        <Link to={menuItem.path}>{menuItem.title}</Link>
-                    </Menu.Item>
-                ))
-            }else{
-                initValue.push((
-                    <SubMenu key={menuItem.path} icon={menuItem.icon} title={menuItem.title}>
-                        {generateMenuItem(menuItem.children)}
-                    </SubMenu>
-                ))
-            }
-            // 存在子菜单，先创建子菜单，再创建菜单项
-            return initValue
-        }, [])
-    }
-    // 获取当前url的路径
-    const pathname = _window.location.pathname
-    // 设置展开的子菜单
-    const openSubMenu = routeConfig.find(item => item.path === pathname) || {}
+function LeftNav (_window) {
+    const [openKeys, setOpenKeys] = useState('')
+    // 设置选中的菜单项Item
+    let path = _window.location.pathname
+    if(path === '/') {path = '/home'}
+    // 当path变化时，修改openKeys
+    useEffect(() => {
+        const subMenu = routeConfig.find(item => item.path === path) || {}
+        setOpenKeys(subMenu.id)
+    }, [path])
+
     return(
         <>
         <div className="logo" >
             <WechatOutlined className="icon"/>微信签署
         </div>
         <Menu 
-            // 初始化选中的二级菜单
-            defaultOpenKeys={[openSubMenu.id]} 
-            // 初始化选中的菜单项
-            defaultSelectedKeys={['/home']}
-            // 当前选中的菜单项
-            selectedKeys={[pathname]}
+            // 选中的菜单项 Menu.Item
+            selectedKeys={[path]}
+            // 打开的子菜单 Menu.SubMenu
+            openKeys={[openKeys]}
             mode="inline" 
             style={{backgroundColor:'#fafafa'}}
         >
             {/* 生成左侧导航内容 ReactNodes */}
-            { generateMenuItem(menuConfig) }
+            { generateMenuItem(menuConfig, setOpenKeys) }
         </Menu>
         </>
     )
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,26 @@
-export default function Home () {
+import { Link } from 'react-router-dom'
+import { Breadcrumb } from 'antd';
+import {
+    FieldTimeOutlined,
+    FileAddOutlined,
+    FileWordOutlined,
+    FileDoneOutlined,
+} from '@ant-design/icons';
+
+import './home.less'
+
+export default function Home(_window) {
     return (
-        <div>首页</div>
+        <>
+            <Breadcrumb separator=">" style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>快捷操作</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="card-style">
+                <Link className="item" to="/newfile"><FileAddOutlined className="icon-1 icon" /><span>新建空白面签</span></Link>
+                <Link className="item" to="/upload"><FileWordOutlined className="icon-2 icon" /><span>上传本地文件</span></Link>
+                <Link className="item" to="/ongoing"><FieldTimeOutlined className="icon-3 icon" /><span>正在签署的面签</span></Link>
+                <Link className="item" to="/complete"><FileDoneOutlined className="icon-4 icon" /><span>已经结束的面签</span></Link>
+            </div>
+        </>
     )
 }
