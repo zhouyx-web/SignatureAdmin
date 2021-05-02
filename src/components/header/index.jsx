187c5bf66b718c9e0bb8844dfc05ab23b822d1ba
zyx <1376672363@qq.com>
2021-05-02 13:45:17 +0800

    头部导航条搭建

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,47 @@
-export default function HeaderInner () {
-    return(
-        <div>HeaderInner</div>
+import React, {useState} from 'react'
+import {
+    Layout,
+    Avatar,
+    Modal,
+    Popover,
+} from 'antd'
+import { UserOutlined, ExclamationCircleOutlined  } from '@ant-design/icons';
+
+import './index.less'
+
+const { Header } = Layout
+const { confirm } = Modal
+
+const DetailContent = (
+    <>
+        <p>用户中心</p>
+        <p onClick={showConfirm}>退出登录</p>
+    </>
+)
+function showConfirm() {
+    confirm({
+      title: '确认退出登录吗？',
+      icon: <ExclamationCircleOutlined />,
+      content: '当前登录用户:Username',
+      onOk() {
+        console.log('OK');
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+
+
+export default function HeaderInner() {
+    return (
+    <>
+        <Header className="site-layout-background">
+            <Popover content={DetailContent} trigger="hover">
+                <Avatar className="avatar" size="large" icon={<UserOutlined />} />
+            </Popover>
+            <span>Username</span>
+        </Header>
+    </>
     )
 }
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -3,12 +3,12 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { Layout } from 'antd'
 
 import LeftNav from '../../components/left-nav/index'
-import HeaderInner from '../../components/header/index'
+import Header from '../../components/header/index'
 import routeConfig from '../../config/routeConfig'
 
-const { Header, Content, Footer, Sider } = Layout
+const { Content, Footer, Sider } = Layout
 const generateRouteList = routeConfig => {
-  return routeConfig.map(menuItem => (<Route {...menuItem}/>)) 
+  return routeConfig.map(menuItem => (<Route key={menuItem.path} {...menuItem}/>)) 
 }
 
 export default function Admin (){
@@ -16,12 +16,10 @@ export default function Admin (){
   return (
       <Layout style={{ minHeight: '100vh' }} >
       <Sider style={{backgroundColor:'#fafafa'}} breakpoint='md' collapsedWidth="0">
-        <LeftNav></LeftNav>
+        <LeftNav/>
       </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0, backgroundColor:'#fafafa' }} >
-          <HeaderInner></HeaderInner>
-        </Header>
+        <Header/>
         <Content style={{ margin: '0 16px' }}>
           <Switch>
             {generateRouteList(routeConfig)}
